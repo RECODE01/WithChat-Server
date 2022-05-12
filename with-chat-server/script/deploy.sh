63f89e8e42327e1f03328103579bb3f59e228e20
choigeon <ck731@naver.com>
2022-05-12 18:34:47 +0900

    change path

--- with-chat-server/script/deploy.sh
+++ with-chat-server/script/deploy.sh
@@ -1,7 +1,8 @@
-ssh -i ~/.ssh/id_rsa withchat01@db.withchat.site <<EOF
+ssh -i ../../.ssh/id_rsa withchat01@db.withchat.site <<EOF
     cd WithChat-Server/with-chat-server	
     git pull origin master
     npm install
     pm2 restart npm
     exit
 EOF
+var/lib/jenkins/workspace/withchat-server/
