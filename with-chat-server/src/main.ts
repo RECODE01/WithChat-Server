12434315e4b598cb4b8c6dfe9252786ace6940d9
Jaenk-99 <jaehy0429@gmail.com>
2022-05-28 12:23:30 +0900

    feat/#41/socketSetting2 first setting

--- with-chat-server/src/main.ts
+++ with-chat-server/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './utils/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,7 +10,6 @@ async function bootstrap() {
     credentials: true,
   });
   setupSwagger(app);
-  app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(3000);
 }
 bootstrap();
