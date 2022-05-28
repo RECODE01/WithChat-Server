16922975a02344ae2db55643f2e3bf51377ee1e3
choigeon <ck731@naver.com>
2022-05-28 12:29:50 +0900

    upload api docs 작성

--- with-chat-server/src/apis/file/file.controller.ts
+++ with-chat-server/src/apis/file/file.controller.ts
@@ -9,15 +9,42 @@ import {
 import { FileService } from './file.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+@ApiTags('파일 업로드 API')
 @Controller('file')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiCreatedResponse({
+    description: '파일 업로드 성공',
+    schema: {
+      example: {
+        success: true,
+        url: `https://storage.googleapis.com/withchat/asdasdasd`,
+      },
+    },
+  })
   @Bind(UploadedFile())
   uploadFile(file: Express.Multer.File, @Res() res: Response) {
-    console.log(file);
     return this.fileService.uploadFile(file, res);
   }
 }
